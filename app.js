d7a07ccfadca86507da25c98bcdc9489ff4e09cf
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-05-01 17:23:06 +0400

    cors update

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { getPaintings } from "./controllers/paintingController.js";
 import connect from "./database/mongo.js";
+import cors from "cors";
 
 dotenv.config();
 connect();
 
 const app = express();
+
+app.use(cors());
 
 app.use("/assets", express.static("./public/assets"));
 
